ea811275853993405e8b8cdcd8db5850a96efa62
Akiya-K <2090618h@stu.kobe-u.ac.jp>
2021-11-29 02:04:56 +0900

    Update Section3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -10,7 +10,20 @@ function setup(){
 
 function draw(){
   background(160, 192, 255);
-  count = (count + 1) % cycle;
-  // BLANK[1]
-  ellipse(width / 2, height / 2, size);
+  if(keyIsDown(" ".charCodeAt(0))){
+  count = (count + 2) % cycle;
+}
+  else{
+    count = (count + 1) % cycle;
+  }
+
+  if(count > 25  && count < 50 ){
+   ellipse(width / 2, height / 2, (50-count)*4);
+}
+  else if(count < 25){
+   ellipse(width / 2, height / 2, count*4);
+ }
+  else{
+   ellipse(width/2, height/2, 25);
+  }
 }
